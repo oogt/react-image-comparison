0738acd9ba9a9f1b58322ccc19d636a092a5415d
Robbert van Caem <robbertvancaem@gmail.com>
2019-05-07 11:25:24 +0200

    Updates

---
+++ precache-manifest.7c00c78a7ab2b758c266b63aaa262fe6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8840e46b5c7f3437893ef10a8d628daf",
+    "url": "/react-image-comparison/index.html"
+  },
+  {
+    "revision": "576ceb6dc131d56daed9",
+    "url": "/react-image-comparison/static/css/main.5677dc66.chunk.css"
+  },
+  {
+    "revision": "37919bd5e90b105f5cc0",
+    "url": "/react-image-comparison/static/js/2.8e878cdc.chunk.js"
+  },
+  {
+    "revision": "576ceb6dc131d56daed9",
+    "url": "/react-image-comparison/static/js/main.ae217493.chunk.js"
+  },
+  {
+    "revision": "ff155f68143a875b7c3c",
+    "url": "/react-image-comparison/static/js/runtime~main.e7e5b1e2.js"
+  },
+  {
+    "revision": "8bb4b9083833ce4c52c7da060b988797",
+    "url": "/react-image-comparison/static/media/bottle-base.8bb4b908.png"
+  },
+  {
+    "revision": "7e4d0abc384a15271838111a65ff9e54",
+    "url": "/react-image-comparison/static/media/bottle-blue.7e4d0abc.png"
+  },
+  {
+    "revision": "5c0db6950766b11d67917aeef8394f5a",
+    "url": "/react-image-comparison/static/media/bottle-green.5c0db695.png"
+  },
+  {
+    "revision": "2e396ee0d35cd8930137c88705a71406",
+    "url": "/react-image-comparison/static/media/bottle-pink.2e396ee0.png"
+  },
+  {
+    "revision": "c759cc276d20d7f261e6eb63e5623ff2",
+    "url": "/react-image-comparison/static/media/bottle-yellow.c759cc27.png"
+  }
+]);
